d1aae9aa0cf2c9487e25d6f63e796397b394e2c2
Yid1331 <81423727+Yid1331@users.noreply.github.com>
2022-06-20 17:21:06 +0100

    Delete Time Tunnel directory

--- Time Tunnel/Tools/set_uplugin_var.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM 1 = .uplugin file path
-REM 2 = parameter name
-REM 3 = new parameter value
-powershell.exe -nologo -noprofile -executionpolicy bypass %~dp0set_uplugin_var.ps1 %1 %2 %3
